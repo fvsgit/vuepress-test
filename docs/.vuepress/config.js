3c7f10a52cda61ad637895ea72f78a2120e6c5d3
Francois van Staden <francoisvans@gmail.com>
2022-06-23 13:56:58 +0200

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,4 +2,13 @@ module.exports = {
     lang: 'en-US',
     title: 'Hello, VuePress!',
     description: 'This is my first VuePress site',
+    theme: defaultTheme({
+        // default theme config
+        navbar: [
+            {
+                text: 'Home',
+                link: '/',
+            },
+        ],
+    })
 }
