90ffe66a7e717fc851649644f5187190017a3941
fvsgit <19401449+fvsgit@users.noreply.github.com>
2022-06-23 14:26:03 +0200

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,5 +13,19 @@ module.exports = {
                 link: '/',
             },
         ],
+        sidebar: {
+          '/guide/': [
+            {
+              text: 'Guide',
+              children: ['/guide/README.md', '/guide/getting-started.md'],
+            },
+          ],
+          '/reference/': [
+            {
+              text: 'Reference',
+              children: ['/reference/cli.md', '/reference/config.md'],
+            },
+          ],
+        },
     })
 }
